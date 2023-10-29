9a3b2bdb7bc21c1c9455bcbfae0e8665df0b7f0b
backlitbytes <149135318+backlitbytes@users.noreply.github.com>
2023-10-29 08:58:54 -0700

    Add favicon, try adding twitter tags

--- src/stores/carts.ts
+++ src/stores/carts.ts
@@ -14,16 +14,16 @@ export interface CartData {
 const createCartStore = () => {
 	const { subscribe, update } = writable<Array<CartData>>([
 		{ hue: 0, sat: 100, scene: 'Red', selected: false, unlocked: true },
-		{ hue: 30, sat: 100, scene: 'Orange', selected: false, unlocked: true },
-		{ hue: 60, sat: 100, scene: 'Yellow', selected: false, unlocked: true },
-		{ hue: 120, sat: 100, scene: 'Green', selected: false, unlocked: true },
-		{ hue: 240, sat: 60, scene: 'Blue', selected: false, unlocked: true },
-		{ hue: 270, sat: 100, scene: 'Purple', selected: false, unlocked: true },
-		{ hue: 0, sat: 0, scene: 'Black', selected: false, unlocked: true },
-		{ hue: 0, sat: 0, scene: 'White', selected: false, unlocked: true },
-		{ hue: 0, sat: 100, scene: 'RainbowForrest', selected: false, unlocked: true },
-		{ hue: 0, sat: 100, scene: 'RainbowVillage', selected: false, unlocked: true },
-		{ hue: 0, sat: 100, scene: 'RainbowOcean', selected: false, unlocked: true }
+		{ hue: 30, sat: 100, scene: 'Orange', selected: false, unlocked: false },
+		{ hue: 60, sat: 100, scene: 'Yellow', selected: false, unlocked: false },
+		{ hue: 120, sat: 100, scene: 'Green', selected: false, unlocked: false },
+		{ hue: 240, sat: 60, scene: 'Blue', selected: false, unlocked: false },
+		{ hue: 270, sat: 100, scene: 'Purple', selected: false, unlocked: false },
+		{ hue: 0, sat: 0, scene: 'Black', selected: false, unlocked: false },
+		{ hue: 0, sat: 0, scene: 'White', selected: false, unlocked: false },
+		{ hue: 0, sat: 100, scene: 'RainbowForrest', selected: false, unlocked: false },
+		{ hue: 0, sat: 100, scene: 'RainbowVillage', selected: false, unlocked: false },
+		{ hue: 0, sat: 100, scene: 'RainbowOcean', selected: false, unlocked: false }
 	]);
 
 	return {
